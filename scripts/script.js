f3eb55e4a61afc4e49507092aeb778a5bb55083d
80009-kalil-zahr <kalilzahr20@gmail.com>
2021-04-21 23:06:20 -0300

    listo

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 import * as Distribucion from "./Distribucion.js"
 
-
 // console.log(Distribucion.uniforme(6, 5, 8));
 // console.log(Distribucion.exponencial(6, 4));
 // console.log(Distribucion.normalBoxMuller(5, 5, 1));
@@ -14,6 +13,7 @@ window.distribucionSeleccionada = function distribucionSeleccionada() {
     var elem = document.getElementById("selector");
     cambiarDistribucionActiva(elem.value);
     $("#rellenar").hide();
+    $("#chartdiv").hide();
 }
 
 
@@ -31,30 +31,89 @@ function cambiarDistribucionActiva(id) {
     }
 }
 
-function obtenerInputs() {
+function obtenerInputs(sacarIntervalo) {
     var inputs = document.getElementsByClassName("textbox activo");
     var res = [];
     for (var item of inputs) {
-        res.push(item.value);
+        res.push(Number(item.value));
+    }
+    if(sacarIntervalo) {
+        res.splice(-1,1);
     }
     return res;
 }
 
+function obtenerIntervalos(subintervalos, min, max) {
+    var amplitud = ((max-min) / subintervalos) + 0.0001;
+    var listaIntervalos = [];
+    for(var i=0; i<subintervalos; i++) {
+        var inicio = min + amplitud * i;
+        var cierre = min + amplitud * (i+1);
+        listaIntervalos.push([inicio, cierre]);
+    }
+    // listaIntervalos[listaIntervalos.length-1[1]] += 0.01;
+    return listaIntervalos;
+}
+
+function obtenerInputIntervalo() {
+    var cantidadIntervalos = document.getElementsByClassName("textbox intervalos activo")[0];
+    return cantidadIntervalos ? cantidadIntervalos.value : -1;
+}
+
+function frecuencia(listaNumeros, listaIntervalos) {
+    var frecuencias = [];
+    for(var i=0; i<listaIntervalos.length; i++) {
+        frecuencias.push(0);
+    }
+    for(var i=0; i<listaNumeros.length; i++) {
+        for(var j=0; j<listaIntervalos.length; j++) {
+            if(listaNumeros[i] >= listaIntervalos[j][0] && listaNumeros[i] < listaIntervalos[j][1]) {
+                frecuencias[j]++;
+                break;
+            }
+        }
+    }
+    return frecuencias;
+}
+
+
+function frecuenciaPoisson(outputs) {
+    var max = Math.max(...outputs);
+    var min = Math.min(...outputs);
+    var frecuenciasObservadas = new Array(max-min+1).fill(0);
+    for(var i=0; i<outputs.length; i++) {
+        frecuenciasObservadas[(outputs[i]-min)]++;
+    }
+    return frecuenciasObservadas;
+}
 
 window.mostrarOutput = function mostrarOutput() { // Cuando cambio de distribucion quiero que desaparezca la tabla anterior y genere una nueva
-    // generar();
-    rellenarTabla();
-    mostrarGrafico();
+    var inputs = obtenerInputs();
+    var cantidadIntervalos = obtenerInputIntervalo();
+    var outputs = generar();
+    var min = Math.min(...outputs);
+    var max = Math.max(...outputs);
+    // console.log("MIN " + min + "  MAX " + max);
+    var listaIntervalos = obtenerIntervalos(cantidadIntervalos, min, max);
+    var frecuenciasObservadas = cantidadIntervalos!=-1 ? frecuencia(outputs, listaIntervalos) : frecuenciaPoisson(outputs);
+    rellenarTabla(outputs);
+    var intervalosString = [];
+    for(var i=0; i<listaIntervalos.length; i++) {
+        intervalosString.push(listaIntervalos[i][0].toFixed(2).toString() + "-" + listaIntervalos[i][1].toFixed(2).toString());
+    }
+    // console.log(listaIntervalos);
+    // console.log(frecuenciasObservadas);
+    mostrarGrafico(intervalosString, frecuenciasObservadas, outputs);
 }
 
 
 function generar() {
-    var inputs = obtenerInputs();
+    var distribucionSeleccionada = document.getElementById("selector").value;
+    var inputs = obtenerInputs(distribucionSeleccionada != "input-poisson");
     var outputs = []
     for(var i=0; i<inputs.length; i++) {
         inputs[i] = Number(inputs[i]);
     }
-    var distribucionSeleccionada = document.getElementById("selector").value;
     if(distribucionSeleccionada == "input-uniforme") {
         outputs = Distribucion.uniforme(inputs);
     }
@@ -74,9 +133,8 @@ function generar() {
 }
 
 
-function rellenarTabla() {
+function rellenarTabla(outputs) {
     var cadena = "<tr class='titulo-tabla'><th>i</th><th>número</th></tr>";
-    var outputs = generar();
     for(var i=0; i<outputs.length; i++) {
         cadena += "<tr><td>" + (i+1) + "</td><td>" + outputs[i] + "</td></tr>"
     }
@@ -85,6 +143,119 @@ function rellenarTabla() {
 }
 
 
-function mostrarGrafico() {
+function mostrarGrafico(listaIntervalos, frecuenciasObservadas, outputs) {
+    am4core.ready(function() {
 
+        // Themes begin
+        am4core.useTheme(am4themes_dark);
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+        
+        
+        
+        var chart = am4core.create('chartdiv', am4charts.XYChart)
+        chart.colors.step = 2;
+        
+        chart.legend = new am4charts.Legend()
+        chart.legend.position = 'top'
+        chart.legend.paddingBottom = 20
+        chart.legend.labels.template.maxWidth = 95
+        
+        var xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+        xAxis.dataFields.category = 'category'
+        xAxis.renderer.cellStartLocation = 0.1
+        xAxis.renderer.cellEndLocation = 0.9
+        xAxis.renderer.grid.template.location = 0;
+        
+        var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        yAxis.min = 0;
+        
+        function createSeries(value, name) {
+            var series = chart.series.push(new am4charts.ColumnSeries())
+            series.dataFields.valueY = value
+            series.dataFields.categoryX = 'category'
+            series.name = name
+        
+            series.events.on("hidden", arrangeColumns);
+            series.events.on("shown", arrangeColumns);
+        
+            var bullet = series.bullets.push(new am4charts.LabelBullet())
+            bullet.interactionsEnabled = false
+            bullet.dy = 30;
+            bullet.label.text = '{valueY}'
+            bullet.label.fill = am4core.color('#ffffff')
+        
+            return series;
+        }
+        
+        chart.data = [
+            {
+                category: "",
+                first: 0
+            }
+        ]
+
+        if(listaIntervalos.length > 0) {
+            for(var i=0; i<listaIntervalos.length; i++) {
+                chart.data[i] = {
+                    category: listaIntervalos[i],
+                    first: frecuenciasObservadas[i]
+                }
+            }
+        } else {
+            // outputs.sort().filter((item, index) => outputs.indexOf(item) === index);
+            var min = Math.min(...outputs);
+            console.log(min);
+            console.log("FR " + frecuenciasObservadas)
+            for(var i=0; i<frecuenciasObservadas.length; i++) {
+                chart.data[i] = {
+                    category: i+min, 
+                    first: frecuenciasObservadas[i]
+                }
+            }
+        }
+        
+        
+        createSeries('first', 'Frecuencias Observadas');
+        
+        function arrangeColumns() {
+        
+            var series = chart.series.getIndex(0);
+        
+            var w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
+            if (series.dataItems.length > 1) {
+                var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+                var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+                var delta = ((x1 - x0) / chart.series.length) * w;
+                if (am4core.isNumber(delta)) {
+                    var middle = chart.series.length / 2;
+        
+                    var newIndex = 0;
+                    chart.series.each(function(series) {
+                        if (!series.isHidden && !series.isHiding) {
+                            series.dummyData = newIndex;
+                            newIndex++;
+                        }
+                        else {
+                            series.dummyData = chart.series.indexOf(series);
+                        }
+                    })
+                    var visibleCount = newIndex;
+                    var newMiddle = visibleCount / 2;
+        
+                    chart.series.each(function(series) {
+                        var trueIndex = chart.series.indexOf(series);
+                        var newIndex = series.dummyData;
+        
+                        var dx = (newIndex - trueIndex + middle - newMiddle) * delta
+        
+                        series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                        series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                    })
+                }
+            }
+        }
+        
+        }); // end am4core.ready()
+        $("#chartdiv").show();
 }
